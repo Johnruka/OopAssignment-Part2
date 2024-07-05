b72d8abed4a1034f8fd2fb1d55bee33edf649d6e
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-05 11:30:19 +0200

    Modified

--- src/main/java/se/lexicon/Person.java
+++ src/main/java/se/lexicon/Person.java
@@ -1,12 +1,24 @@
 package se.lexicon;
 
-public class Person {
+public abstract class Person {
 
     private final String name;
     private final AppRole role;
+    private String username;
+    private final String password;
 
-    public Person(String name, AppRole role){
+    public Person(String name, AppRole role,String username, String password){
         this.name = name;
         this.role = role;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
     }
 }
