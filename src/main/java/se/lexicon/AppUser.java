8fbe91f536a34e573c6ff69bf23979178c35e38d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-05 12:39:45 +0200

    Modified

--- src/main/java/se/lexicon/AppUser.java
+++ src/main/java/se/lexicon/AppUser.java
@@ -5,4 +5,35 @@ public class AppUser {
     private String username;
     private String password;
     private AppRole role;
+    private String credentials;
+
+    public AppUser(String username, AppRole role, String password) {
+        this.role = role;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AppRole getRole() {
+        return role;
+    }
+
+    public void setRole(AppRole role) {
+        this.role = role;
+    }
 }
