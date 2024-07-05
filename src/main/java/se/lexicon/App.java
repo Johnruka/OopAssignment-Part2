b5daef98e5b7189eff766d6c9ce779dee15b3564
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-05 16:23:50 +0200

    Modified

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,11 +1,24 @@
 package se.lexicon;
 
 
+import java.util.Arrays;
+
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
+
+        Person[] user = {
+                new Person("John Baptist", "user"),
+                new Person("David Land", "admin"),
 
 
+        };
+        Person person = new Person(user);
 
+        System.out.print(Arrays.equals(user, user));
+
+
+    }
 }
 
-}
+
+
