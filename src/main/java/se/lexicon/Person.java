ff14b0c7e08d929955b92c60b014bb64601b3051
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-05 11:15:27 +0200

    Modified

--- src/main/java/se/lexicon/Person.java
+++ src/main/java/se/lexicon/Person.java
@@ -3,4 +3,10 @@ package se.lexicon;
 public class Person {
 
     private final String name;
+    private final AppRole role;
+
+    public Person(String name, AppRole role){
+        this.name = name;
+        this.role = role;
+    }
 }
